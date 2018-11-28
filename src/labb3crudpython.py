951a47f602e64f816a7515ff6155ca707768ffee
Titti <lisakristinmaria@outlook.com>
2018-11-28 09:38:09 +0100

    skrivit in email o namn

---
+++ src/labb3crudpython.py
@@ -0,0 +1,18 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    print "Hello World"
+
+
+from OrmCrud import OrmCrud
+crud = OrmCrud()
+crud.add("Majsorm")
+crud.add("Huggorm")
+print(crud.get_snakes())
+
+crud.delete("Snok")
+crud.update("Mask", "Kobra")
+print(crud.get_snakes())
+exit()
