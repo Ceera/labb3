9ee6d965d806e62be16096f8445ceb32ceb2d489
Micke Knutes <mickekristiansen@gmail.com>
2018-11-28 14:02:43 +0100

    commited some changes

--- src/OrmCrud.py
+++ src/OrmCrud.py
@@ -1,5 +1,27 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
 
 
+class OrmCrud:
+    
+    #Konstruktor
+        def__init__(self):
+        #instansvariable that is unique  for every instance created through this class
+    self.name = []#A list with all snakes
+    
+    #often soa is the first param to method self. It is a great codeconvention but has no meaning for python
+    def add(self, name):
+        self.names.append(name)
+        
+    def delete(self,name):
+        if(name in self.names):
+            self.names.remove(name)
+            
+    def update(self,old_name,new_name):
+        for key, value in enumerate(self.names):
+            if(value==old_name):
+                self.names[key]=new_name
+                break
+    
+    def get_snakes(self):
+        return self.names
+    
+    
