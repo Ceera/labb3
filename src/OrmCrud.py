105a8a82e608582d1520e472738685aac95f6ac6
Micke Knutes <mickekristiansen@gmail.com>
2018-11-29 19:16:58 +0100

    took the files back

---
+++ src/OrmCrud.py
@@ -0,0 +1,35 @@
+
+
+class OrmCrud:
+    from OrmObj import OrmObj
+    #Konstruktor
+    def __init__(self):
+        #instansvariable that is unique  for every instance created through this class
+            
+            self.snakes = [] 
+        
+    
+    #often soa is the first param to method self. It is a great codeconvention but has no meaning for python
+    def add(self, OrmObj):
+        self.snakes.append(OrmObj)
+        
+    def delete(self, id):
+        for x in self.snakes:
+            if(id == x.id):
+                self.snakes.remove(x)
+            
+    def update(self, id, name , temperament, color, length):
+         for x in self.snakes:
+            if(id == x.id):
+                x.name = name
+                x.temeprament = temperament
+                x.color = color
+                x.length = length
+                #OrmObj.updateSnake(id, name, temperament, color, length)
+                
+                
+           
+    def get_snakes(self):
+        return self.snakes
+    
+    
---
+++ src/labb3crudpython.py
@@ -0,0 +1,55 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+from OrmCrud import OrmCrud
+from OrmObj import OrmObj
+from operator import attrgetter
+
+crud = OrmCrud()
+OrmObj1 = OrmObj(1, "Mask", "Nice", "Black", 5)
+crud.add(OrmObj1)
+OrmObj2 = OrmObj(2, "Hussorm", "Angry", "Black", 25)
+crud.add(OrmObj2)
+OrmObj3 = OrmObj(3, "Majsorm", "Nice", "Green", 55)
+crud.add(OrmObj3)
+
+lista = crud.get_snakes()
+
+for i in lista:
+    print(i.getSnake())
+
+crud.delete(1)
+
+print("Deletar nr 1")
+
+listaAfterDelete = crud.get_snakes()
+
+for j in listaAfterDelete:
+    print(j.getSnake())
+    
+print("Nu ska huggorm uppdateras till Huggmask")
+    
+crud.update(2, "Huggmask", "Hangry", "Red", 2)
+
+listaAfterUpdate = crud.get_snakes()
+
+for k in listaAfterUpdate:
+    print(k.getSnake())
+    
+    
+print("\n")
+print("Sorterar listan på längd fallande:")
+snakes_sort=crud.get_snakes()
+snakes_sort.sort(key=attrgetter('length'),reverse=True)
+
+array = crud.get_snakes()
+for i in array:
+    print(i.getSnake())
+    
+    
+print(lista.index(2))
+ 
+
+exit()
